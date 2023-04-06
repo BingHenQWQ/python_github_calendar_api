49c23fb385faf6a3fdd298a063cf597a257428d1
三两 <112811285+BingHenQWQ@users.noreply.github.com>
2023-04-06 09:58:32 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ import json
 def list_split(items, n):
     return [items[i:i + n] for i in range(0, len(items), n)]
 def getdata(name):
-    gitpage = requests.get("https://github.com/" + name)
+    gitpage = requests.get("https://github.com/BingHenQWQ" + name)
     data = gitpage.text
     datadatereg = re.compile(r'data-date="(.*?)" data-level')
     datacountreg = re.compile(r'data-count="(.*?)" data-date')
